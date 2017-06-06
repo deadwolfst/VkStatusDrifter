18826ece9a9630c7e916a2261327b6727c616c56
Kirill <whobscr@gmail.com>
2017-06-07 01:13:45 +0300

    changed status message

--- src/main/java/com/vk/api/examples/oauth/user/Drifter.java
+++ src/main/java/com/vk/api/examples/oauth/user/Drifter.java
@@ -54,7 +54,7 @@ public class Drifter implements Runnable {
                 "/bin/bash",
                 "-c",
                 "uptime -p | awk '{ print $1, $2, $3, $4, substr($5, 0, length($5)-1); };'; cat $HOME/status;" +
-                        " echo '. '; fortune -n 70 -s;"
+                        " echo '. '; fortune -n 50 -s;"
             };
             vk.status().set(actor)
                 .text("server " + execute(command))
