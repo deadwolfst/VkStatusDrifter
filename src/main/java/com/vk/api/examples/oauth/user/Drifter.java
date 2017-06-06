639b13639658be7b24558798c636c65c9e8a144b
Kirill <whobscr@gmail.com>
2017-06-07 01:30:30 +0300

    changed status message

--- src/main/java/com/vk/api/examples/oauth/user/Drifter.java
+++ src/main/java/com/vk/api/examples/oauth/user/Drifter.java
@@ -1,37 +1,14 @@
 package com.vk.api.examples.oauth.user;
 
-import static java.util.concurrent.TimeUnit.*;
-import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
 import com.vk.api.sdk.client.VkApiClient;
 import com.vk.api.sdk.client.actors.UserActor;
-import com.vk.api.sdk.httpclient.HttpTransportClient;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import org.jsoup.Jsoup;
-import org.jsoup.Connection;
-import org.jsoup.Connection.Response;
-import org.jsoup.Connection.Method;
-import org.jsoup.helper.Validate;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLDecoder;
-import java.io.InputStream;
-import java.util.Properties;
 import java.util.Map;
-import java.util.HashMap;
-import java.util.regex.Pattern;
-import java.util.Calendar;
+import java.util.Properties;
 
 public class Drifter implements Runnable {
-
-    //private static Map.Entry<Integer, String> credentials;
     private static UserActor actor;
     private static VkApiClient vk;
     private static String clientId;
@@ -57,12 +34,13 @@ public class Drifter implements Runnable {
                         " echo -n '. Here's your hf-hour aphorism: '; /usr/games/fortune -n 50 -s;"
             };
             vk.status().set(actor)
-                .text("server " + execute(command))
+                .text(execute(command))
                 .execute();
         } catch (Exception e) {
             System.out.println("Exception: " + e.toString());
             try {
-                Map.Entry<Integer, String> credentials = CredentialsManager.authenticate(clientId, clientEmail, clientPass, properties);
+                Map.Entry<Integer, String> credentials =
+                        CredentialsManager.authenticate(clientId, clientEmail, clientPass, properties);
                 actor = new UserActor(credentials.getKey(), credentials.getValue());
                 run();
             } catch (Exception ue) {
@@ -74,16 +52,15 @@ public class Drifter implements Runnable {
     }
 
     private static String execute(String[] command) {
-        StringBuffer output = new StringBuffer();
+        StringBuilder output = new StringBuilder();
         Process p;
         try {
             p = Runtime.getRuntime().exec(command);
             p.waitFor();
-            BufferedReader reader =
-                new BufferedReader(new InputStreamReader(p.getInputStream()));
-            String line = "";
+            BufferedReader reader = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            String line;
             while ((line = reader.readLine())!= null) {
-                output.append(line + "\n");
+                output.append(line).append("\n");
             }
         } catch (Exception e) {
             e.printStackTrace();
