b8850550bf3dcbcd89eacbcdb6dbe293c747c869
Kirill <whobscr@gmail.com>
2017-06-08 11:21:10 +0300

    status message changed

--- src/main/java/com/vk/api/examples/oauth/user/Drifter.java
+++ src/main/java/com/vk/api/examples/oauth/user/Drifter.java
@@ -30,9 +30,10 @@ public class Drifter implements Runnable {
         String counterFilename = "$HOME/drifter/aphorism-counter.number";
         int vkStatusMaxCharacters = 140;
 
-        final String statusCommand = String.format("uptime -p | awk '{ print $1, $2, $3, $4, " +
-                        "substr($5, 0, length($5)-1); };'; more %s;" +
-                        " echo -n \". Profound nonsense #$(more %s): \";"/* +
+        final String statusCommand = String.format(
+                "uptime -p | awk '{ print $1, $2, $3, $4, " +
+                        "substr($5, 0, length($5)-1); };'; more %s;" /*+
+                        " echo -n \". Profound nonsense #$(more %s): \";" +
                         "echo $(($(more %s) + 1)) > %s; /usr/games/fortune -n 50 -s;"*/,
                 statusFilename, counterFilename, counterFilename, counterFilename);
         try {
